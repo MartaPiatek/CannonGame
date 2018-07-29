413b92d4dcc0408f248f5d7e0c390060a5ac8196
Marta Piątek <martusia.piatek@gmail.com>
2018-07-29 16:58:59 +0200

    Dodanie klasy Blocker

---
+++ app/src/main/java/pl/martapiatek/cannongame/Blocker.java
@@ -0,0 +1,15 @@
+package pl.martapiatek.cannongame;
+
+public class Blocker extends GameElement {
+
+    private int missPenalty; // kara za trafienie w przeszkodę
+
+    public Blocker(CannonView view, int color, int missPenalty, int x, int y, int width, int length, float velocityY) {
+        super(view, color, CannonView.BLOCKER_SOUND_ID, x, y, width, length, velocityY);
+        this.missPenalty = missPenalty;
+    }
+
+    public int getMissPenalty() {
+        return missPenalty;
+    }
+}
